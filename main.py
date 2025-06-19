58868b325f4d360dd755bf730cd450c2a723d366
Zaque Neves <zaquemnevess@gmail.com>
2025-06-19 16:46:03 -0300

    Teste de maturidade basicão

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
 print("Hello World!")
+
+msg_sem_maturidade = "Tu tem maturidade pra porra nenhuma!"
+msg_maturidade = "Agora sim tu tem maturidade neste caralho, porra!"
+
+def main():
+    maturidade = False
+    while not maturidade:
+        resposta = input("Você tem maturidade? (s/n): ").strip().lower()
+        if resposta == 's':
+            maturidade = True
+            print(msg_maturidade)
+        elif resposta == 'n':
+            print(msg_sem_maturidade)
+            break
+        else:
+            print("Responde direito, porra! É 's' ou 'n'!")
+
+if __name__ == "__main__":
+    main()
